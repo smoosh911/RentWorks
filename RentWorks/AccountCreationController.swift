5e1e725deda7b6c73d69a06cb813d324b9f122b5
Spencer Curtis <spencercolecurtis@gmail.com>
2016-11-06 22:51:09 -0700

    Revert "Quick fix to solve an compile failure"
    This reverts commit 460b80f73e4ee4713bf4219a92f3ef277ecd2a6f.
    
    # Conflicts:
    #	RentWorks/AccountCreationController.swift

--- RentWorks/AccountCreationController.swift
+++ RentWorks/AccountCreationController.swift
@@ -25,7 +25,13 @@ class AccountCreationController {
         let nextVC = pageVC.renterVCs[currentVCIndex + 1]
         guard !currentRenterVCs.contains(nextVC) else { return }
         currentRenterVCs.append(nextVC)
+<<<<<<< HEAD
 //        AccountCreationController.resetRenterPageVCDataSourceFor(renterVC: renterVC)
+=======
+        AccountCreationController.resetRenterPageVCDataSourceFor(renterVC: renterVC)
+        pageVC.setViewControllers([self], direction: .forward, animated: true, completion: nil)
+        
+>>>>>>> parent of 460b80f... Quick fix to solve an compile failure
     }
     
     static func resetRenterPageVCDataSourceFor(renterVC: UIViewController) {
