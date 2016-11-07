9a5ff5aec776f66d1718d0036514015213e5768d
Spencer Curtis <spencercolecurtis@gmail.com>
2016-11-06 22:59:10 -0700

    .gitignore is now working

--- Pods/Target Support Files/Pods-RentWorks/Pods-RentWorks-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RentWorks : NSObject
-@end
-@implementation PodsDummy_Pods_RentWorks
-@end
--- Pods/Target Support Files/Pods-RentWorksUITests/Pods-RentWorksUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RentWorksUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RentWorksUITests
-@end
