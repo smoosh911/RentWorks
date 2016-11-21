a8ec2e1b49973e001265b9467b78e93213d8cf69
smoosh911 <perrymichaelscott@gmail.com>
2016-11-20 20:30:55 -0700

    Add credit rating picker and new storyboards for landlords and renters that inherit from main

---
+++ RentWorks/LandlordSettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LandlordSettingsViewController.swift
+//  RentWorks
+//
+//  Created by Michael Perry on 11/20/16.
+//  Copyright © 2016 Michael Perry. All rights reserved.
+//
+
+import Foundation
