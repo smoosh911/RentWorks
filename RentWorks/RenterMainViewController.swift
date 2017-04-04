695d816649ab5f1f0bedddb6fa06244f01aff544
Eric Clinger <cling.eric@gmail.com>
2017-04-04 17:59:14 -0600

    Renter Profile View Controller

--- RentWorks/RenterMainViewController.swift
+++ RentWorks/RenterMainViewController.swift
@@ -13,6 +13,7 @@ class RenterMainViewController: MainViewController, RenterFilterSettingsModalVie
     
     // MARK: outlets
     
+    
     @IBOutlet weak var vwLoadingNewCards: UIView!
     @IBOutlet weak var vwNoMoreCards: UIView!
     
@@ -108,7 +109,17 @@ class RenterMainViewController: MainViewController, RenterFilterSettingsModalVie
         }
         
     }
-    
+    @IBAction func userProfileViewButton(_ sender: Any) {
+        if FIRAuth.auth()?.currentUser == nil
+        {
+            print("Not logged in")
+            performSegue(withIdentifier: "segueToSignUpProfileVC", sender: self)
+        }
+        else{
+            print("Logged in")
+            performSegue(withIdentifier: "segueToProfile", sender: self)
+        }
+    }
     // MARK: Swipableview delegate
     
     override func updateCardUI() {
---
+++ RentWorks/RenterProfileViewController.swift
@@ -0,0 +1,121 @@
+//
+//  RenterProfileViewController.swift
+//  RentWorks
+//
+//  Created by Eric Clinger on 3/20/17.
+//  Copyright © 2017 Michael Perry. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseAuth
+
+class RenterProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var modalView: UIView!
+    @IBOutlet weak var profileNavBarView: UIView!
+    @IBAction func renterProfileCancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+
+    }
+    
+    
+    let accountArray = ["Edit Profile","Change Password","Credit Score"]
+    
+    let aboutArray = ["Contact Us","Privacy Policy","Terms of Service"]
+    
+    let lastArray = ["Log Out"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return accountArray.count
+        case 1:
+            return aboutArray.count
+        case 2:
+            return lastArray.count
+        default:
+            return 0
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        var currentOption: String
+        
+        switch indexPath.section {
+        case 0:
+            currentOption = accountArray[indexPath.row]
+        case 1:
+            currentOption = aboutArray[indexPath.row]
+        case 2:
+            currentOption = lastArray[indexPath.row]
+        default:
+            currentOption = ""
+        }
+        
+        cell.textLabel!.text = currentOption
+        if(indexPath.section == 0 || indexPath.section == 1)
+        {
+            cell.accessoryType = .disclosureIndicator
+        }
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "ACCOUNT"
+        case 1:
+            return "ABOUT US"
+        case 2:
+            return ""
+        default:
+            return ""
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected row: \(indexPath.row) in section \(indexPath.section)")
+        if indexPath.row == 0 && indexPath.section == 2
+        {
+            let manager = FBSDKLoginManager()
+            manager.logOut()
+            
+            do {
+                try FIRAuth.auth()?.signOut()
+            } catch let error as NSError {
+                log(error)
+                }
+            
+            for renter in MatchController.matchedRentersForProperties {
+                UserDefaults.standard.set(0, forKey: "\(Identifiers.UserDefaults.propertyMatchCount.rawValue)/\(renter.key)")
+            }
+            UserDefaults.standard.set(0, forKey: Identifiers.UserDefaults.renterMatchCount.rawValue)
+            MatchController.isObservingCurrentUserLikeEndpoint = false
+            MatchController.matchedProperties = []
+            MatchController.matchedRentersForProperties = [:]
+            UserController.propertyFetchCount = 0
+            UserController.renterFetchCount = 0
+            
+            let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "loginVC")
+            self.present(loginVC, animated: true, completion: nil)
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        modalView.layer.cornerRadius = 10
+        //profileNavBarView.layer.cornerRadius = 10
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
